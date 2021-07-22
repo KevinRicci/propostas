48e8e24a185713fbc71b81a10466f0a46e489e5b
Kevin Ricci Hernandes <46302439+KevinRicci@users.noreply.github.com>
2021-07-22 12:50:04 -0300

    Acompanhamento de proposta

--- src/main/java/br/com/brzupacademy/propostas/proposta/PropostaController.java
+++ src/main/java/br/com/brzupacademy/propostas/proposta/PropostaController.java
@@ -7,15 +7,13 @@ import br.com.brzupacademy.propostas.proposta.analiseFinanceira.EnviaProposta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/propostas")
@@ -42,5 +40,14 @@ public class PropostaController {
             return ResponseEntity.created(uri).body(new PropostaResponse(proposta));
         }
         else throw new ApiException(HttpStatus.UNPROCESSABLE_ENTITY, "Já existe uma proposta para esse documento");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PropostaResponse> buscaPorId(@PathVariable Long id){
+        Optional<Proposta> possivelProposta = propostaRepository.findById(id);
+        if(possivelProposta.isPresent()){
+            return ResponseEntity.ok().body(new PropostaResponse(possivelProposta.get()));
+        }
+        else throw new ApiException(HttpStatus.NOT_FOUND, "Proposta não encontrada");
     }
 }
