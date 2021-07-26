947fdfb2d4197ec2320b9881c08d66383f60b378
Kevin Ricci Hernandes <46302439+KevinRicci@users.noreply.github.com>
2021-07-26 18:20:37 -0300

    Logando com OAuth2

---
+++ src/main/java/br/com/brzupacademy/propostas/security/OAuthConfigurations.java
@@ -0,0 +1,35 @@
+package br.com.brzupacademy.propostas.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+@EnableWebSecurity
+public class OAuthConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+    }
+
+    private JwtAuthenticationConverter getJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities");
+        converter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+
+        return authenticationConverter;
+    }
+}
